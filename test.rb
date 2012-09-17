e6094b7d72359afbd74e6f5a5db242977fdf64b2
ito masaya <itomasaya@team-lab.com>
2012-09-17 20:57:41 +0900

    るびーーーーーーー

---
+++ test.rb
@@ -0,0 +1,19 @@
+print("hello!!")
+
+if 1 then
+        print("if 1");
+end
+
+if true then
+        print("if true");
+end
+
+if false then
+        print("if false");
+end
+
+if nil then
+        print("if nil");
+end
+
+
